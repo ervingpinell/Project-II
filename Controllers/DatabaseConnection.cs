cf35254bca8e1d4fb9eea6c26856ff3a2199fbac
DELVER OVETH DUMAS HURTADO <ddumas@ucatolica.ac.cr>
2024-12-03 21:00:12 -0600

    Coneccion DB

---
+++ Controllers/DatabaseConnection.cs
@@ -0,0 +1,16 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data.SqlClient;
+
+namespace Project_II
+{
+    public class DatabaseConnection
+    {
+        private const string connectionString = "Server=localhost;Database=payments_db;Uid=root;Pwd=;";
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
